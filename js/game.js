a36b9fc05203bd32f229c6739e42ea226f680395
Roman Salin <romansalin1990@gmail.com>
2014-05-23 02:41:58 +0400

    Add simple implementation

--- js/game.js
+++ js/game.js
@@ -1,8 +1,21 @@
 var TicTacToe = (function () {
 
-    var gameType = {user: 'x', robot: 'o'};
+    var TURNS = {player: 'x', robot: 'o'};
 
-    var gameMatrix = [[],[],[]];
+    var SIZE = 3;
+
+    var WINNING_COMBINATIONS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    var gameState = [];
 
     var settings = {};
 
@@ -12,68 +25,133 @@ var TicTacToe = (function () {
     };
 
     var getSelections = function () {
-        settings.startNewGameButton = $('#game-new');
-        settings.gameTable = $('#game');
+        settings.startButton = $('#start');
+        settings.gameTable = $('#board');
         settings.gameCell = settings.gameTable.find('td');
     };
 
     var bindEvents = function () {
-        settings.startNewGameButton.on('click', startNewGame);
+        settings.startButton.on('click', startNewGame);
         settings.gameCell.on('click', userMove);
     };
 
     var startNewGame = function () {
-        gameMatrix = [[],[],[]];
+        for (var i = 0; i < SIZE * SIZE; i++) {
+            gameState[i] = null;
+        }
 
         settings.gameCell.find('span').remove();
     };
 
     var userMove = function () {
-        move(this, gameType.user);
+        var row = $(this).closest('tr').index();
+        var col = $(this).index();
+
+        var cell = row * SIZE + col;
+
+        if (gameState.length === 0) {
+            startNewGame();
+        }
+
+        move(cell, TURNS.player);
         resolveRobotMove();
     };
 
-    var move = function (cell, gameType) {
-        var row = $(cell).closest('tr').index();
-        var col = $(cell).index();
+    var move = function (cell, turn) {
 
-        if (gameMatrix[row][col] == null) {
-            gameMatrix[row][col] = gameType;
+        if (gameState[cell] === null) {
+            gameState[cell] = turn;
 
-            $(cell).html('<span>' + gameType + '</span>');
+            console.log(gameState);
 
-            console.log(gameMatrix);
+            var gameCell = settings.gameTable.find('tr:eq(' + Math.floor(cell / SIZE) + ') td:eq(' + cell % SIZE + ')');
+            $(gameCell).html('<span>' + turn + '</span>');
         }
     };
 
     var resolveRobotMove = function () {
-        var row = 1;
-        var col = 2;
-
-        var chain = [];
+        var winner = checkGameEnd();
+        if (winner) {
+            return;
+        }
 
         //TODO
-        for (var i = 0; i < gameMatrix.length; i++) {
-            chain[i] = 0;
 
-            for (var j = 0; j < gameMatrix.length; j++) {
-                if (gameMatrix[i][j] == gameType.user) {
-                    chain[i]++;
+
+
+        var cell = 8;
+
+        move(cell, TURNS.robot);
+    };
+
+    var checkGameEnd = function () {
+        for (var i = 0; i < WINNING_COMBINATIONS.length; i++) {
+            var match = {win: 0, lose: 0};
+
+            for (var j = 0; j < WINNING_COMBINATIONS[i].length; j++) {
+                if (gameState[WINNING_COMBINATIONS[i][j]] === TURNS.player) {
+                    match.win++;
+                }
+
+                if (gameState[WINNING_COMBINATIONS[i][j]] === TURNS.robot) {
+                    match.lose++;
                 }
             }
 
-            if (chain[i] == 3) {
-                showResult(true);
-            } else if (chain[i] == 2)
+            if (match['win'] === SIZE) {
+                showResult(1, WINNING_COMBINATIONS[i]);
+
+                return true;
+            }
+
+            if (match['lose'] === SIZE) {
+                showResult(-1, WINNING_COMBINATIONS[i]);
+
+                return true;
+            }
         }
 
-        var cell = settings.gameTable.find('tr:eq(' + row + ') td:eq(' + col + ')');
+        for (var i = 0; i < SIZE * SIZE; i++) {
+            if (gameState[i] === null) {
 
-        move(cell, gameType.robot);
+                return false;
+            }
+        }
+
+        return 0;
     };
 
-    var showResult = function (isWin) {
-        isWin ? alert('Вы выиграли!') : alert('Вы проиграли!');
+    var showResult = function (result, cells) {
+        var message;
+        var cellClass;
+
+        switch (result) {
+            case -1:
+                cellClass = 'lose';
+                message = 'Вы проиграли!';
+                break;
+            case 0:
+                message = 'Ничья!';
+                break;
+            case 1:
+                cellClass = 'win';
+                message = 'Вы выиграли!';
+                break;
+        }
+
+        if (typeof cellClass !== 'undefined') {
+            for (var i = 0; i < cells.length; i++) {
+                var gameCell = settings.gameTable.find('tr:eq(' + Math.floor(cells[i] / SIZE) + ') td:eq(' + cells[i] % SIZE + ')');
+                $(gameCell).find('span').addClass('win');
+            }
+        }
+
+        message += ' Хотите сыграть еще?';
+
+        var y = confirm(message);
+        if (y == true) {
+            location.reload(true);
+        }
     };
 
     return {
